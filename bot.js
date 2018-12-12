6c2d075a0db0a2c2d12ff3e753604487da57c4f7
EAGA <6077173+EAGA@users.noreply.github.com>
2018-12-12 10:43:10 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,14 +6,9 @@ client.on('ready', () => {
 })
 
 client.on("message", message => {
-    if (message.content == "ping") {
+    if (message.content == "!ping") {
         message.reply("pong");
     }
 });
-
-// Get your bot's secret token from:
-// https://discordapp.com/developers/applications/
-// Click on your application -> Bot -> Token -> "Click to Reveal Token"
-bot_secret_token = "NTIyMjg3MzU1NTA0MzYxNDcz.DvIxzw.eMm83SV5BeHaUg1HnmguIQS7PMg"
 
 client.login(process.env.BOT_TOKEN);
